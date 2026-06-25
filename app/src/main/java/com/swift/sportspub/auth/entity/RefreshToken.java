ca834b59598e87d42efd44cabbe7f3f92c19b6c5
juyangjin <wndid2008@gmail.com>
2026-06-25 18:47:44 +0900

    REFACTOR : token_hash 쪽 타입 문제 수정

--- app/src/main/java/com/swift/sportspub/auth/entity/RefreshToken.java
+++ app/src/main/java/com/swift/sportspub/auth/entity/RefreshToken.java
@@ -47,7 +47,7 @@ public class RefreshToken extends BaseEntity {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @Column(name = "token_hash", nullable = false, length = 64, columnDefinition = "CHAR(64)")
+    @Column(name = "token_hash", nullable = false, length = 64)
     private String tokenHash;
 
     @Column(name = "expires_at", nullable = false)
