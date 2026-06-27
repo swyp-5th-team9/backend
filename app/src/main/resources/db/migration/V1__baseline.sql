b5cca7e691950e0a37b3158c0fadaf6b916fb1f6
juyangjin <wndid2008@gmail.com>
2026-06-28 02:04:53 +0900

    Merge branch 'develop' of github.com:swyp-5th-team9/backend into feat/#26-favorite-domain

--- app/src/main/resources/db/migration/V1__baseline.sql
+++ app/src/main/resources/db/migration/V1__baseline.sql
@@ -323,18 +323,3 @@ CREATE TABLE report_images (
     CONSTRAINT fk_report_images_report
         FOREIGN KEY (report_id) REFERENCES reports(report_id) ON DELETE CASCADE
 );
-
--- ============================================================================
--- 시드: KBO 10개 구단 (sport_type='KBO', short_name = 크롤러 매핑 키)
--- ============================================================================
-INSERT INTO teams (sport_type, name, short_name, home_stadium) VALUES
-    ('KBO', 'LG 트윈스',     'LG',   '잠실야구장'),
-    ('KBO', '두산 베어스',   '두산', '잠실야구장'),
-    ('KBO', 'KT 위즈',       'KT',   '수원KT위즈파크'),
-    ('KBO', 'SSG 랜더스',    'SSG',  '인천SSG랜더스필드'),
-    ('KBO', 'NC 다이노스',   'NC',   '창원NC파크'),
-    ('KBO', 'KIA 타이거즈',  'KIA',  '광주-기아챔피언스필드'),
-    ('KBO', '롯데 자이언츠', '롯데', '사직야구장'),
-    ('KBO', '삼성 라이온즈', '삼성', '대구삼성라이온즈파크'),
-    ('KBO', '한화 이글스',   '한화', '대전한화생명이글스파크'),
-    ('KBO', '키움 히어로즈', '키움', '고척스카이돔');
